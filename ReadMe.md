99df1ee582707be2cb6c45ee7f6dcb7acab4f35b
lij520 <472798773@qq.com>
2019-09-10 16:50:30 +0800

    修改

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+1、dev_onemap:地图开发
+
+2、dev_htmljs:前端css+js+html
+
+3、dev_showSystem:大屏展示
+
+4、dev_react:react全站项目
+
+5、dev_roadII:二期项目代码
--- src/components/linkButton/index.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Button } from 'antd';
-import './index.less';
-
-
-export default function LinkButton(props){
-    return <Button {...props} className='link-button'></Button>
-}
